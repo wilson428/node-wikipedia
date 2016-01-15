3ffacb84d1eb51410e65ed341a83436fef2898e6
Chris Wilson <christopher.e.wilson@gmail.com>
2016-01-15 11:39:00 -0500

    added a random article getter

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.categories = require(__dirname + "/lib/categories");
 module.exports.revisions = require(__dirname + "/lib/revisions");
 module.exports.page = require(__dirname + "/lib/page");
+module.exports.random = require("./lib/random");

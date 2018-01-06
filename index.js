13b87a0a64dae0db692a833c999c4c4f99991f33
bomelino <tino.hartmann@gmail.com>
2018-01-06 13:43:47 +0100

    added setEndpoint(url) to index.js so the user can set the endpoint to other wikis, e.g. http://de.wikipedia.org/w/api.php

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ module.exports.categories = require(__dirname + "/lib/categories");
 module.exports.revisions = require(__dirname + "/lib/revisions");
 module.exports.page = require(__dirname + "/lib/page");
 module.exports.random = require("./lib/random");
+module.exports.setEndpoint = function(url){
+    require(__dirname +"/lib/dial").endpoint = url
+}
